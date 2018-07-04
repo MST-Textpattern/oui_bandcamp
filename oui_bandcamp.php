a69a4adaf412acda3dd0294cad4f0c0a363fb639
Nicolas Morand <contact@nicolasmorand.com>
2018-07-04 12:21:51 +0200

    Provider dedicated tags are now declared in the parent class

--- oui_bandcamp.php
+++ oui_bandcamp.php
@@ -87,13 +87,3 @@ namespace Oui {
         }
     }
 }
-
-namespace {
-    function oui_bandcamp($atts) {
-        return oui_player(array_merge(array('provider' => 'bandcamp'), $atts));
-    }
-
-    function oui_if_bandcamp($atts, $thing) {
-        return oui_if_player(array_merge(array('provider' => 'bandcamp'), $atts), $thing);
-    }
-}
